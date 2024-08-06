35c9a3c3a45935454aa0dd0d60b9da76c1115dd3
Italo Marcelo <MarceloDev2000@outlook.com>
2024-08-06 19:04:34 -0300

    update 1.1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,14 @@ export default function Destination() {
       <>
         <div id="Home">  
           <section className='Home-space'>
+             <div>
              <h1 className='space--title'>So, you want to travel to
-             Space</h1>
+             </h1>
+             <h2  className='space--sub--title'>Space</h2>
              <p className='sub--space--text'>Let’s face it; if you want to go to space, you might as well genuinely go to 
   outer space and not hover kind of on the edge of it. Well sit back, and relax 
   because we’ll give you a truly out of this world experience!</p>
+             </div>
             <div className='bubble-area'>
                <div className='bubble'>
                  <h2 className='bubble-title'>Explore</h2>
