407a8538f2584a4a88abff8de93eea13daf46050
Italo Marcelo <MarceloDev2000@outlook.com>
2024-08-16 10:32:25 -0300

    update 1.7 fix

--- src/components/Tec/TecItens/TecItens.jsx
+++ src/components/Tec/TecItens/TecItens.jsx
@@ -6,7 +6,6 @@ export default function TecItem(props) {
         <div className='slide-image-item'>
         <div className='full'></div>
            <img src={props.img} alt='image' className='full mobile' id={props.id} />
-           <img src={props.img2} alt='image' className='full tablet' id={props.id} />
         </div>
         <div className='card-content text-center '>
            <h2 className='card-content-title up--Text'>
